bf9a5d163344322d022dbe8100569ffebabe3437
Kevin Agnes <kevinagnes@gmail.com>
2022-11-08 16:39:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,107 @@
+const THREE = window.MINDAR.IMAGE.THREE;
+import {mockWithVideo, mockWithImage} from './libs/camera-mock.js';
+import {loadGLTF} from "./libs/loader.js";
+import { Vector3, } from './libs/three.js-r132/build/three.module.js';
+import { RGBELoader } from './libs/three.js-r132/examples/jsm/loaders/RGBELoader.js';
+console.log(THREE);
+
+let anchors = [];
+const models = [
+    ["./static/models/ExpoAR_Scenes/Guerra/Guerra.gltf", "Guerra"],
+    ["./static/models/ExpoAR_Scenes/Adoracao/Adoracao.gltf", "Adoracao"],
+    ["./static/models/ExpoAR_Scenes/Boqueirao/Boqueirao.gltf", "Boqueirao"],
+    ["./static/models/ExpoAR_Scenes/Veado/Veado.gltf", "Veado"],
+    ["./static/models/ExpoAR_Scenes/Beijo/Beijo.gltf", "Beijo"],
+    ["./static/models/ExpoAR_Scenes/Acrobatas/Acrobatas.gltf", "Acrobatas"],
+    ["./static/models/ExpoAR_Scenes/CacadaOnca/CacadaOnca.gltf", "CacadaOnca"],
+    ["./static/models/ExpoAR_Scenes/CavernaCacador/CavernaCacador.gltf", "CavernaCacador"],
+    ["./static/models/ExpoAR_Scenes/Desfiladeiro/Desfiladeiro.gltf", "Desfiladeiro"],
+    ["./static/models/ExpoAR_Scenes/PedraFurada/PedraFurada.gltf", "PedraFurada"]
+]; 
+const transforms = [
+    // scale, rotation, position
+    [new Vector3(1.25,1.25,1.25),new THREE.Euler(0,0,0),new Vector3(0,0,0)],
+    [new Vector3(1.5,1.5,1.5),new THREE.Euler(0,0,0),new Vector3(0,0,0)],
+    [new Vector3(1.5,1.5,1.5),new THREE.Euler(0,0,0),new Vector3(0,0,0)],
+    [new Vector3(1.5,1.5,1.5),new THREE.Euler(0,0,0),new Vector3(0,0,0)],
+    [new Vector3(1.5,1.5,1.5),new THREE.Euler(0,0,0),new Vector3(0,0,0)],
+    [new Vector3(1.5,1.5,1.5),new THREE.Euler(0,0,0),new Vector3(0,0,0)],
+    [new Vector3(1.5,1.5,1.5),new THREE.Euler(0,0,0),new Vector3(0,0,0)],
+    [new Vector3(1.75,1.75,1.75),new THREE.Euler(Math.PI/2,0,0),new Vector3(0,0.2,0)],
+    [new Vector3(2,2,2),new THREE.Euler(Math.PI/2,Math.PI/2,0),new Vector3(0,0.2,0)],
+    [new Vector3(1.5,1.5,1.5),new THREE.Euler(Math.PI/2,0,0),new Vector3(0,0.2,0)]
+]
+
+
+async function LoadModelAttachToAnchorIndex(mindarThree, transform, model, index){
+    // This is the anchor of the Marker Index 0
+    const anchor = mindarThree.addAnchor(index);
+    // Loading GLTF
+    const gltf = await loadGLTF(model[0]);
+    gltf.scene.transform
+    gltf.scene.scale.copy(transform[0]);
+    gltf.scene.rotation.copy(transform[1]);
+    gltf.scene.position.copy(transform[2]);
+    // Adding gltf1 to marker index 0 group
+    anchor.group.add(gltf.scene);
+    //anchors.push(anchor);
+    anchor.onTargetFound = () => {
+        console.log("Found anchor: "+ index +", name: " + model[1]);
+    };
+    anchor.onTargetLost = () => {
+        console.log("Lost anchor: "+ index +", name: " + model[1]);
+    };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const start = async () => {
+        //mockWithImage();
+        //mockWithVideo("./static/mov/test.mp4");
+
+        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+            container: document.body,
+            imageTargetSrc: './static/target/targets.mind',
+            uiLoading: "yes",
+            uiScanning: "yes",
+            // uiError: "yes",
+            // filterMinCF:0, 
+            // filterBeta:0, 
+            // warmupTolerance:0, 
+            // missTolerance:0,
+            //maxTrack:2
+        });
+
+        console.log(mindarThree);
+
+        const {renderer,scene,camera} = mindarThree;
+
+        AddLight(scene);
+
+        for (let i=0;i<10;i++) {
+            await LoadModelAttachToAnchorIndex(mindarThree, transforms[i], models[i],i);
+        }
+
+        await mindarThree.start();
+
+        renderer.setAnimationLoop(() => {
+            renderer.render(scene, camera);
+        });
+    }
+    start();
+});
+
+function AddLight(scene){
+    new RGBELoader()
+    .setPath('./static/hdri/')
+    .load('01.hdr', function ( texture ) {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        let skyboxMat = new THREE.MeshBasicMaterial({
+        map: texture,
+        side: THREE.BackSide,
+        fog: false,
+        depthWrite: false,
+        });
+        //scene.background = texture;
+        scene.environment = texture;
+    }); 
+}
